bdabfa468f6a8760aec10e900d919ea6ffaf782c
Alfred <ajmolero@gmail.com>
2017-01-21 20:27:10 -0500

    Create populate.js
    Script to insert 1MM sample documents.

---
+++ populate.js
@@ -0,0 +1,9 @@
+for (i = 0; i < 100; i++) {
+	for(j = 0; j < 100; j++) {
+		x = [];
+		for (k = 0; k < 100; k++) {
+			x.push( { a : i, b : j, c: k, _id : ( 100 * 100 * i + 100 * j + k ) } );
+		}
+		db.example.insert(x);
+	}
+}
