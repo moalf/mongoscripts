9c51c8f7416267665a69366bb5dd480f85e6fa25
Alfred <ajmolero@gmail.com>
2017-02-05 13:25:44 -0500

    Correcting object names

--- bulk_load.js
+++ bulk_load.js
@@ -1,16 +1,16 @@
 var load = {
-	"loadOrdered" : function () {
+	"ordered" : function () {
 		for (i = 0; i < 100; i++) {
 			for(j = 0; j < 100; j++) {
 				x = [];
 				for (k = 0; k < 100; k++) {
 					x.push( { a : i, b : j, c: k, _id : ( 100 * 100 * i + 100 * j + k ) } );
 				}
-				db.example.insert(x);
+				db.example.insert(x, { ordered : true });
 			}
 		}
 	},
-	"loadUnordered" : function () {
+	"unordered" : function () {
 		for (i = 0; i < 100; i++) {
 			for(j = 0; j < 100; j++) {
 				x = [];
