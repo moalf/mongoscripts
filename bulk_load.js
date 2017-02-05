4b7e0a02c65f78050d22ebfaf3572c6fd095ab97
Alfred <ajmolero@gmail.com>
2017-02-05 13:22:21 -0500

    Create bulk_load.js

---
+++ bulk_load.js
@@ -0,0 +1,24 @@
+var load = {
+	"loadOrdered" : function () {
+		for (i = 0; i < 100; i++) {
+			for(j = 0; j < 100; j++) {
+				x = [];
+				for (k = 0; k < 100; k++) {
+					x.push( { a : i, b : j, c: k, _id : ( 100 * 100 * i + 100 * j + k ) } );
+				}
+				db.example.insert(x);
+			}
+		}
+	},
+	"loadUnordered" : function () {
+		for (i = 0; i < 100; i++) {
+			for(j = 0; j < 100; j++) {
+				x = [];
+				for (k = 0; k < 100; k++) {
+					x.push( { a : i, b : j, c: k, _id : ( Math.floor(Math.random()*1000) * 100 * 100 * i + 100 * j + k ) } );
+				}
+				db.example.insert(x, { ordered : false });
+			}
+		}
+	}	
+}
